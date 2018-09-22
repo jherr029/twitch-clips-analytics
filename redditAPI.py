46c4c4414e6313ec512794f3f6ab76b2f17d3e2d
Jonathan <jherr029@ucr.edu>
2018-09-22 10:37:18 -0700

    added basic insert to streamer table

--- redditAPI.py
+++ redditAPI.py
@@ -1,11 +1,13 @@
 import praw 
 import json
 import logging
+import MySQLdb
 from twitch import TwitchClient
 from pathlib import Path
+from collections import OrderedDict
 
 
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
 
 #TODO: check if api access is true
@@ -24,8 +26,27 @@ def getTwitchAPIAccess():
     twitch = TwitchClient(twID)
     return twitch
 
-def getFrontPage(reddit):
-    twitch = getTwitchAPIAccess()
+def connectDB():
+    home = str(Path.home())
+    home = home + '/.sql'
+
+    mid = open(home)
+
+    vec = []
+    for line in mid: 
+        vec.append(line)
+
+    # reason for the [:-1] is to remove \n
+    mydb = MySQLdb.connect(
+        host=vec[0][:-1],
+        #user=vec[1],
+        passwd=vec[1],
+        db='lsf'
+    )
+
+    return mydb
+
+def getFrontPage(reddit, db, twitch):
 
     vector = []
 
@@ -38,12 +59,24 @@ def getFrontPage(reddit):
 
             streamer, sid, clipViews  = getClipInfo(twitch, slug)
             followers, views = getChannelInfo(twitch, sid)
-            vector.append((streamer, sid, followers, views, slug, submission.score, clipViews))
+
+            d = OrderedDict()
+            d['streamer'] = streamer
+            d['sid'] = sid
+            d['followers'] = followers
+            d['views'] = views
+            d['slug'] = slug
+            d['score'] = submission.score
+            d['clip views'] = clipViews
+
+            insertTOStreamerTable(db, d)
+            vector.append(d)
     
     print("------")
 
     for x in vector:
         print(x)
+        print()
 
     return vector
 
@@ -62,10 +95,42 @@ def getChannelInfo(twitch, id):
 
     return channel.followers, channel.views
 
+def createStreamerTable(db):
+    cursor = db.cursor()
+
+    sqlTable = """CREATE TABLE IF NOT EXISTS STREAMERS (
+                    CHANNEL VARCHAR(20) NOT NULL,
+                    ID          INT NOT NULL,
+                    FOLLOWERS   INT NOT NULL,
+                    VIEWS       INT NOT NULL )"""
+    
+    cursor.execute(sqlTable)
+
+def insertTOStreamerTable(db, d):
+    cursor = db.cursor()
+
+    sql = "INSERT INTO STREAMERS(CHANNEL, ID, FOLLOWERS, VIEWS) \
+            VALUES ( '%s', '%s', '%s', '%s')" % \
+            (d['streamer'], d['sid'], d['followers'], d['views'])
+
+    try:
+        cursor.execute(sql)
+        db.commit()
+
+    except:
+        db.rollback()
+
+
 def main():
+    db = connectDB()
     reddit = getRedditAPIAccess()
     twitch = getTwitchAPIAccess()
 
-    dataVector = getFrontPage(reddit)
+    dataVector = getFrontPage(reddit, db, twitch)
+
+
+    createStreamerTable(db)
+
+    #print(dataVector[0]['streamer'])
 
 main()
