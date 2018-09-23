555f7e174582a76687872f3d320a812d98ada303
Jonathan <jherr029@ucr.edu>
2018-09-22 18:55:19 -0700

    added channel overview tables

--- redditAPI.py
+++ redditAPI.py
@@ -52,31 +52,35 @@ def getFrontPage(reddit, db, twitch):
 
     if (reddit.read_only):
         subreddit = reddit.subreddit('livestreamfail')
-        for submission in subreddit.hot(limit=10):
+        for submission in subreddit.hot(limit=25):
             logging.info('Title: %s - Score: %s', submission.title, submission.score)
-            slug = submission.url[24:]
-            logging.info('Slug: %s', slug)
 
-            streamer, sid, clipViews  = getClipInfo(twitch, slug)
-            followers, views = getChannelInfo(twitch, sid)
+            if 'clips.twitch.tv' in submission.url:
+                slug = submission.url[24:]
+                logging.info('Slug: %s', slug)
 
-            d = OrderedDict()
-            d['streamer'] = streamer
-            d['sid'] = sid
-            d['followers'] = followers
-            d['views'] = views
-            d['slug'] = slug
-            d['score'] = submission.score
-            d['clip views'] = clipViews
+                streamer, sid, title, clipViews, clipDate  = getClipInfo(twitch, slug)
+                followers, views = getChannelInfo(twitch, sid)
 
-            insertTOStreamerTable(db, d)
-            vector.append(d)
+                d = OrderedDict()
+                d['streamer'] = streamer
+                d['sid'] = sid
+                d['followers'] = followers
+                d['views'] = views
+                d['slug'] = slug
+                d['score'] = submission.score
+                d['title'] = title
+                d['clip views'] = clipViews
+                d['clip date'] = clipDate
+
+                insertTOStreamerTable(db, d)
+                vector.append(d)
     
     print("------")
 
-    for x in vector:
-        print(x)
-        print()
+    # for x in vector:
+    #     print(x)
+    #     print()
 
     return vector
 
@@ -84,9 +88,12 @@ def getClipInfo(twitch, slug):
 
     clip = twitch.clips.get_by_slug(slug)
 
-    logging.info('Streamer: %s - ID: %s -  Clip Views: %s', clip.broadcaster['name'], clip.broadcaster['id'], clip.views)
+    #print(clip)
+    logging.info('Streamer: %s - ID: %s - Title: %s - Clip Views: %s - Datetime: %s', clip.broadcaster['name'], 
+                    clip.broadcaster['id'], clip.title, clip.views, clip.created_at)
 
-    return clip.broadcaster['name'], clip.broadcaster['id'], clip.views 
+    return clip.broadcaster['name'], clip.broadcaster['id'], clip.title, clip.views, clip.created_at
+
 
 def getChannelInfo(twitch, id):
     channel = twitch.channels.get_by_id(id)
@@ -98,18 +105,33 @@ def getChannelInfo(twitch, id):
 def createStreamerTable(db):
     cursor = db.cursor()
 
-    sqlTable = """CREATE TABLE IF NOT EXISTS STREAMERS (
-                    CHANNEL VARCHAR(20) NOT NULL,
+    sqlTable = """CREATE TABLE IF NOT EXISTS channels (
+                    CHANNEL VARCHAR(30) NOT NULL,
                     ID          INT NOT NULL,
                     FOLLOWERS   INT NOT NULL,
                     VIEWS       INT NOT NULL )"""
     
     cursor.execute(sqlTable)
 
+    # the following will not work if there are non unique entries in the table
+
+    constraint = "ALTER TABLE channels \
+                    ADD CONSTRAINT UC_streamer UNIQUE (channel, id)"
+
+    try:
+        cursor.execute(constraint)
+        db.commit()
+    
+    except:
+        db.rollback()
+        print('Error: Unable to add constraints')
+
 def insertTOStreamerTable(db, d):
     cursor = db.cursor()
 
-    sql = "INSERT INTO STREAMERS(CHANNEL, ID, FOLLOWERS, VIEWS) \
+    # TODO: has map to check if table exist
+
+    sql = "INSERT INTO channels(CHANNEL, ID, FOLLOWERS, VIEWS) \
             VALUES ( '%s', '%s', '%s', '%s')" % \
             (d['streamer'], d['sid'], d['followers'], d['views'])
 
@@ -119,17 +141,87 @@ def insertTOStreamerTable(db, d):
 
     except:
         db.rollback()
+        #print('Error: Unable to add to streamer table')
+    
 
+def duplicateCheck(db):
+    cursor = db.cursor()
+
+    sql = "SELECT channel, COUNT(channel) \
+            FROM channels \
+            GROUP BY channel \
+            HAVING COUNT(channel) > 1"
+
+    try:
+        cursor.execute(sql)
+
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    
+    except:
+        print('Error: Unable to fetch data')
+
+def clipTables(reddit, twitch, db, dataVector):
+
+    for channel in dataVector:
+        streamer = channel['streamer']
+        slug = channel['slug']
+        clip = channel['title']
+        date = channel['clip date']
+
+        createChannelOverviewTable(streamer, db)
+        insertChannelOverviewTable(streamer, clip, slug, date, db)
+
+def createChannelOverviewTable(streamer, db):
+    cursor = db.cursor()
+    # TODO: has map to check if table exist
+
+    sql = """CREATE TABLE IF NOT EXISTS """ + streamer + "(\
+                CLIP    VARCHAR(200) NOT NULL UNIQUE, \
+                SLUG    VARCHAR(100), \
+                DATE    DATE NOT NULL, \
+                TIME    TIME NOT NULL )"
+    
+    try:
+        cursor.execute(sql)
+
+    except:
+        print('Could not create table')
+    
+def insertChannelOverviewTable(streamer, clip, slug, date, db):
+
+
+    cursor = db.cursor()
+
+    clip = clip.replace('\'', '')
+    print('Attempting', streamer, '-', clip, date)
+
+
+    sql = "INSERT INTO " + streamer + "(CLIP, SLUG, DATE, TIME) \
+            VALUES ( '%s', '%s', '%s', '%s' )" % \
+            (clip, slug, date.date(), date.time())
+
+    try:
+        cursor.execute(sql)
+        db.commit()
+    
+    except:
+        db.rollback()
+        print('Error: Unable to create overview table\n')
+        
 
 def main():
     db = connectDB()
     reddit = getRedditAPIAccess()
     twitch = getTwitchAPIAccess()
 
+    createStreamerTable(db)
     dataVector = getFrontPage(reddit, db, twitch)
 
+    clipTables(reddit, twitch, db, dataVector)
 
-    createStreamerTable(db)
+    #duplicateCheck(db)
 
     #print(dataVector[0]['streamer'])
 
