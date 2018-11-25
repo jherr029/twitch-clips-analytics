3e894cab914cf0ddbd3478a04c1a75c5227a3c45
Jonathan <jherr029@ucr.edu>
2018-11-25 11:19:55 -0800

    improved cmakelist for test and src. Added simple test for curl

--- test/test.hpp
+++ test/test.hpp
@@ -1,5 +1,7 @@
-// #include "../../includes/curl.h"
+#include "../includes/curl.h"
 #include <gtest/gtest.h>
+#include <iostream>
+#include <string>
 
 namespace {
 
@@ -13,6 +15,11 @@ TEST(environmental_variables, ifExist)
     EXPECT_STRNE("", getenv("MYSQL_SERVER_REMOTE"));
     EXPECT_STRNE("", getenv("MYSQL_LOGIN"));
     EXPECT_STRNE("", getenv("MYSQL_PASS"));
+}
+
+TEST(curlGetJsonReddit, successful)
+{
+    ASSERT_STRNE("", curlGetJsonReddit().c_str());
 }
 
 }
